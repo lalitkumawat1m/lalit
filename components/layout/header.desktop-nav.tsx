dd6a85af36467f6fa99ebd8594f4c2274838b205
Lalit Kumawat <lalitkumawat1129@gmail.com>
2024-02-06 13:38:39 +0530

    ts ignore in header section

--- components/layout/header.desktop-nav.tsx
+++ components/layout/header.desktop-nav.tsx
@@ -11,25 +11,26 @@ export const DesktopNav: FC = () => {
   return (
     <>
       <nav className="sm:scrollbar-none header-nav group relative isolate mt-auto hidden h-full justify-center overflow-auto px-2 md:flex">
-        <HoverEffect />
+        <HoverEffect />  
         {HEADER.nav
-          .filter(({ desktop }) => desktop)
+        // @ts-ignore
+        .filter(({ desktop }) => desktop)
           .map((link, i) => {
-            const isActive = router.asPath.split(/[#?]/)[0] === link.href;
+            const isActive = router.asPath.split(/[#?]/)[0] === link!.href;
             return (
               <div
                 className="my-auto flex h-full items-center px-2"
-                key={link.href + link.title + i}
+                key={link!.href + link!.title + i}
               >
                 <Link
-                  href={link.href}
+                  href={link!.href}
                   className={clsx(
                     "relative z-10 flex rounded-md border-2 border-transparent py-1.5 px-4 text-gray-500 outline-none transition-all hfa:text-gray-900 hfa:outline-none d:text-gray-300 d:hfa:text-gray-50",
                     isActive &&
                       "border-gray-700/5 bg-gray-100 bg-clip-padding d:border-gray-50/10 d:bg-gray-800 group-hfa:bg-gray-200/30"
                   )}
                 >
-                  <span className="text-sm font-medium ">{link.title}</span>
+                  <span className="text-sm font-medium ">{link!.title}</span>
                 </Link>
               </div>
             );
